0fe41e55636c353689032cc4de7a527ac6628360
Joao Amaral <joaovillanova@hotmail.com.br>
2020-10-11 13:29:29 -0300

    Endpoint /clientes/id disponivel

--- src/main/java/com/example/demo/domain/Cliente.java
+++ src/main/java/com/example/demo/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.example.demo.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -35,6 +36,7 @@ public class Cliente implements Serializable {
 	
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
